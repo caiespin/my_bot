7deff579570793c7ceb8273e9cafa6e1dab3c97b
Isaac Espinosa <caiespin@ucsc.edu>
2025-02-21 17:19:12 -0800

    Adding joystick functionality - working on robot

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -27,6 +27,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )])
+    )
+
     robot_description = Command('ros2 param get --hide-type /robot_state_publisher robot_description')
 
 
@@ -74,6 +80,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
